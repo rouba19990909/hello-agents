ae85bb2ca41e3fbdae6545b668fce87b8c8bc8a6
cxh <2960497256@qq.com>
2026-08-28 10:53:36 +0800

    2026-8-28 1.BPE添加注释； 2.添加依赖

--- code/chapter3/BPE.py
+++ code/chapter3/BPE.py
@@ -12,10 +12,13 @@ def get_stats(vocab):
 def merge_vocab(pair, v_in):
     """合并词元对"""
     v_out = {}
-    bigram = re.escape(' '.join(pair))
-    p = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
+    bigram = re.escape(' '.join(pair)) # 把元组变成：'p u'
+    # (?<!\S) 前面不是非空白字符，即位于字符串开头或空格后。
+    # (?!\S) 后面不是非空白字符，即位于字符串末尾或空格前。
+    p = re.compile(r'(?<!\S)' + bigram + r'(?!\S)') # 正则表达式对象
     for word in v_in:
-        w_out = p.sub(''.join(pair), word)
+        # 将word中的匹配到的内容替换成合并后的词元
+        w_out = p.sub(''.join(pair), word) # 把两个词元合并成：'pu'
         v_out[w_out] = v_in[word]
     return v_out
 
